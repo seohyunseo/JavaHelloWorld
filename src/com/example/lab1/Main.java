8e7e444d3a43f132ca90d853d4347dde9200b55f
seohyunseo <21700358@handong.edu>
2021-09-04 15:16:48 +0900

    Add asking user's school name

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -9,7 +9,9 @@ public class Main {
 		System.out.println("Hello World!!!\n");
 		System.out.print("이름 입력 : ");
 		String name = s.next();
-		System.out.println("[" + name + "]님 환영합니다~");
+		System.out.print("학교 입력 : ");
+		String school = s.next();
+		System.out.println("[" + school + "]학생, [" + name + "]님 환영합니다~");
 
 	}
 
